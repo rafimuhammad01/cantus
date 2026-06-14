2fcf60b462f6818d4f8b163a699e086b3a98e65e
Rafi Muhammad <rafi10muhammad@gmail.com>
2026-06-14 19:36:13 +0700

    refactor(audio-processor): drop /shift (moved to Go backend)

--- audio-processor/main.py
+++ audio-processor/main.py
@@ -6,12 +6,10 @@ from logging_config import setup_logging
 from routers import melody as melody_router
 from routers import preview_key as preview_key_router
 from routers import separate as separate_router
-from routers import shift as shift_router
 
 setup_logging(os.environ.get("LOG_LEVEL", "info"))
 
 app = FastAPI(title="cantus audio-processor")
-app.include_router(shift_router.router)
 app.include_router(separate_router.router)
 app.include_router(melody_router.router)
 app.include_router(preview_key_router.router)
