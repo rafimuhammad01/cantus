940265229b2beacd74b1da6d6012ae8ade074f5f
Rafi Muhammad <rafi10muhammad@gmail.com>
2026-06-14 19:37:51 +0700

    refactor(audio-processor): drop dead /preview-key endpoint

--- audio-processor/main.py
+++ audio-processor/main.py
@@ -4,7 +4,6 @@ from fastapi import FastAPI
 
 from logging_config import setup_logging
 from routers import melody as melody_router
-from routers import preview_key as preview_key_router
 from routers import separate as separate_router
 
 setup_logging(os.environ.get("LOG_LEVEL", "info"))
@@ -12,7 +11,6 @@ setup_logging(os.environ.get("LOG_LEVEL", "info"))
 app = FastAPI(title="cantus audio-processor")
 app.include_router(separate_router.router)
 app.include_router(melody_router.router)
-app.include_router(preview_key_router.router)
 
 
 @app.get("/health")
