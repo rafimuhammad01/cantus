15a565af2e2d6ea419a2dcfe3c68d898079418bb
Rafi Muhammad <rafi10muhammad@gmail.com>
2026-06-14 19:16:10 +0700

    refactor(audio-processor): drop /search (moved to Go backend)

--- audio-processor/main.py
+++ audio-processor/main.py
@@ -5,14 +5,12 @@ from fastapi import FastAPI
 from logging_config import setup_logging
 from routers import melody as melody_router
 from routers import preview_key as preview_key_router
-from routers import search as search_router
 from routers import separate as separate_router
 from routers import shift as shift_router
 
 setup_logging(os.environ.get("LOG_LEVEL", "info"))
 
 app = FastAPI(title="cantus audio-processor")
-app.include_router(search_router.router)
 app.include_router(shift_router.router)
 app.include_router(separate_router.router)
 app.include_router(melody_router.router)
